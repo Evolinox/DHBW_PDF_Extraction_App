8007ed5186b24fbad48906a828c12f2c3e9d0ed2
Pascal Jedicke <pascal.jedicke@icloud.com>
2024-05-14 17:31:48 +0200

    Improvements

--- App.py
+++ App.py
@@ -1,18 +1,29 @@
 import flet as ft
 import Modules.llm as llm
+import json
+
+jsonContent = {
+  "title": "SAP ist cool!",
+  "student": "Patrick",
+  "firma": "SIT",
+  "gliederung": ["Einleitung", "Was ist SAP?", "Geschichte", "HANA", "UI5", "Meins Meinung"]
+}
+bachelorTestJson = json.dumps(jsonContent)
 
 def main(page: ft.Page):
     page.title = "PDF Extraction App"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    llmText = ft.TextField(value="", text_align=ft.TextAlign.RIGHT, width=500)
+    llmText = ft.Text(value="", text_align=ft.TextAlign.CENTER, width=500)
 
     def getLlmModel(e):
-        llmText.value = llm.analyzeJson('Hey')
+        llmText.value = llm.analyzeJson(bachelorTestJson)
+        page.update()
 
     page.add(
         llmText,
-        ft.IconButton(ft.icons.AIRPLAY_ROUNDED, on_click=getLlmModel)
+        ft.IconButton(ft.icons.SEARCH_ROUNDED, on_click=getLlmModel)
     )
 
 ft.app(main)
--- Modules/llm.py
+++ Modules/llm.py
@@ -1,10 +1,12 @@
 import ollama
+import json
 
 def analyzeJson(jsonObject):
+    jsonData = json.loads(jsonObject)
     response = ollama.chat(model='llama2', messages=[
     {
         'role': 'user',
-        'content': jsonObject,
+        'content': 'I have a Text with the following Title: "' + jsonData['title'] + '", What do you think could be the topic of this Text?',
     },
     ])
     msgContent = response['message']['content']
