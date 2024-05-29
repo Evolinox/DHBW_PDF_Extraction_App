a8097334183a1b8c6a855b8596a28c6823d12f39
FlorianHeck2 <florianheck@mein.gmx>
2024-05-29 21:31:32 +0200

    prepared for further work

--- Extractor.py
+++ Extractor.py
@@ -1,13 +1,10 @@
 from PyPDF2 import PdfReader
-#Imports Imaga-analysation
 import os
-import fitz
-import re
-from PIL import Image 
+import re 
 import json
 folder = "Sample_PDFs"
 filelist = os.listdir(folder)#alle Testdateien
-reader = PdfReader("Sample_PDFs/" + filelist[0])#Fehler: 3;4;5;6;7;8;9;10;13;14;15;19;20;21;22;23;24;26;27;28;29;30;...
+reader = PdfReader("Sample_PDFs/" + filelist[0])#Ausgewählte Datei
 number_of_pages = len(reader.pages)
 page = reader.pages[0]
 text = page.extract_text()
@@ -16,12 +13,14 @@ a = 0
 imagecounter = 0
 location = ""
 totalText = ""
-#Probedaten:
 title = ""
 author = ""
 company = ""
 matNr = ""
 blanktext = ""
+illustrations = 0
+listOfIllustrations = False
+pages = []
 
 def Metadata():
 
@@ -36,22 +35,20 @@ def Metadata():
 #Metadata()
 def extracting():
     global totalText
+    global pages
     a1 = 0
     while a1 < (number_of_pages-1):
         page=reader.pages[a1]
-        print(page.extract_text())
+        #print(page.extract_text())
         totalText += str(page.extract_text())
         a1+=1
-        #print(len(page.images))
-        #imagecounter += page.images
-    print(totalText)
-#extracting()
+        pages += str(page.extract_text())
 
-def getLocation():  
+
+def getLocation(pagecounter):  
         global text
         global location  
         global counter
-        global pagecounter
         global page    
         blanktext = text.replace(" ", "")
         if "Lörrach" in blanktext:
@@ -87,7 +84,7 @@ def getData():
             if not line.isspace():
                 text = text + line + "\n"
         blanktext = text.replace(" ", "")
-        getLocation()
+        getLocation(0)
         
         lines = text.splitlines()
         author = ""
@@ -115,6 +112,24 @@ def getData():
                         author = line.split(":")[1].strip()                        
             else: author = "unknown"
 
+# def Immagecount(page):
+#     print(page)
+#     print("blubber")
+
+# def Listofillustrations():
+#     global pages
+#     global listOfIllustrations
+#     for page in pages:
+#         if "Abbildungsverzeichnis" in page.remove(" ", ""):
+#             if listOfIllustrations == True:
+#                 Immagecount(page)
+#                 break
+#             listOfIllustrations = True
+
+
+
+# Listofillustrations()
+
 def getTitle():
     global title
     title = text.split("helor")[0].replace("\n", "").strip()
@@ -129,17 +144,11 @@ def getTitle():
 def getMatNr():
     global matNr
     matNr = ""
-    if "Matrikelnummer" in blanktext:
+    if "Matrikelnummer" in blanktext or "Matnr" in blanktext:
         lines = blanktext.splitlines()
         for line in lines:
-            if "Matrikelnummer" in line:
-                    
-                #if ":" in line:
-                    matNr = re.findall(r"\d{7}", line)[0]
-                    # match = re.search(r'\d{7}', line)
-                    # if match:
-                    #     matNr = line[:match.start()].strip()
-    print("Matnr: " + matNr)
+            if "Matrikelnummer" in line or "Matnr" in line:
+                    matNr = re.findall(r"\d{7}", line)[0]#search for 7 following digits
 
 def getCompany():
     global company
@@ -154,11 +163,6 @@ def getCompany():
                         company = line.split("irma")[1]
                     elif "Partner" in line:
                         company = line.split("Partner")[1]
-            # if company != "":
-            #     if lines.index(line) < len(lines):
-            #         if re.search('\S', lines[lines.index(line)+1]).start() > 8:
-            #             line = lines[lines.index(line)+1]
-            #             company += line
     company = company.strip()
     if company == "":
         company = "unknown"
@@ -167,88 +171,36 @@ def getCompany():
 
 
 def printinfos():
-    print(author)
-    print(title)
-    print(location)
+    print("author: " + author)
+    print("title: " + title)
+    print("location: " + location)
+    print("Matnr: " + matNr)
+    print("")
 
 counter = 0
-while counter < len(filelist):
-    pagecounter = 0
-    print(counter)
-    print(filelist[counter])
-    reader = PdfReader("Sample_PDFs/" + filelist[counter])
-    page=reader.pages[0]
-    getData()
-    getTitle()
-    getCompany()
-    try:
-        getMatNr()
-    except: 
-        pass
-    printinfos()
-    print("")
-    counter += 1
+# while counter < len(filelist):
+#     pagecounter = 0
+#     print(counter)
+#     print(filelist[counter])
+#     reader = PdfReader("Sample_PDFs/" + filelist[counter])
+#     page=reader.pages[0]
+#     getData()
+#     getTitle()
+#     getCompany()
+#     try:
+#         getMatNr()
+#     except: 
+#         pass
+#     printinfos()
+#     counter += 1
 
-read = PdfReader("Sample_PDFs/" + filelist[19])
-print(read.pages[0].extract_text())
+# read = PdfReader("Sample_PDFs/" + filelist[20])
+# print(read.pages[0].extract_text())
 #print(read.pages[30].extract_text())
 #print(read.pages[2].extract_text())
 #print(read.pages[3].extract_text())
 
 #um zusätzlich erste Seite zu drucken
-
-
-def images():
-    
-    count = 0
-    a=0
-    while a < number_of_pages:
-        page=reader.pages[a]
-        for image_file_object in page.images:
-            with open(str(count) + image_file_object.name, "wb") as fp:
-                fp.write(image_file_object.data)
-                count += 1
-        print("At page " + str(a+1) + " we reached " + str(count)+ " Images")
-        
-
-
-        #Transparentes Bild -> Fehler!!!
-    
-def imagebyMupdf():
-    file = "160907_Bachelorarbeit_Sebastian Sperl.pdf"
-    
-    # open the file 
-    pdf_file = fitz.open(file) 
-    total_images = 0
-    
-    # STEP 3 
-    # iterate over PDF pages 
-    for page_index in range(len(pdf_file)): 
-    
-        # get the page itself 
-        page2 = pdf_file[page_index] 
-        image_list = page2.get_images() 
-       
-    
-        # printing number of images found in this page 
-        if image_list: 
-            print( 
-                f"[+] Found a total of {len(image_list)} images in page {page_index+1}") 
-            total_images = total_images + len(image_list)
-        else: 
-            print("[!] No images found on page", page_index+1) 
-        for image_index, img in enumerate(page2.get_images(), start=1): 
-    
-            # get the XREF of the image 
-            xref = img[0] 
-    
-            # extract the image bytes 
-            base_image = pdf_file.extract_image(xref) 
-            image_bytes = base_image["image"] 
-    
-            # get the image extension 
-            image_ext = base_image["ext"] 
-    print("We have found a total of " + str(total_images) + " images in the file.")
 
 
 def getJson():
@@ -258,7 +210,18 @@ def getJson():
         "totalPages": number_of_pages,
         "firma": company,
         "matNr": matNr,
-        "gliederung": ["Einleitung", "Was ist SAP?", "Geschichte", "HANA", "UI5", "Meins Meinung"],
         "text": totalText
     }
     bachelorTestJson = json.dumps(jsonContent)
+    print(bachelorTestJson)
+
+def runExtraction():  
+    extracting()
+    getData()
+    getTitle()
+    getCompany()
+    try:
+        getMatNr()
+    except:
+        pass
+    getJson()
