53827e58775485a01e59584e2d7412dd54a257ba
Pascal Jedicke <pascal.jedicke@icloud.com>
2024-06-12 01:26:53 +0200

    added some basic NLP Stuff

--- Modules/llm.py
+++ Modules/llm.py
@@ -6,12 +6,16 @@ import ollama
 import json
 
 stop_words = set(stopwords.words("german"))
+test_str = "Bei Gebhardt werden alle Industrie 4.0 Produkte unter dem Namen Galileo IoT zusammengefasst. Darunter fallen Condition Monitoring, Predictive Maintenance, Digitale Zwillinge und generell alle Anwendungsgebiete, die sich mit Datenerhebung oder Auswertung befassen. Kunden bekommen ihre Daten und Auswertungen über eine Online-Plattform bereitgestellt. In diesem Kapitel geht es hauptsächlich um den Aufbau von Galileo Internet of Things (IoT)."
 
 clusteredJson = {
     "data": [
 
     ]
 }
+
+def downloadNltkStuff():
+    nltk.download()
 
 def analyzeJson(jsonObject):
     global jsonData
@@ -21,9 +25,24 @@ def analyzeJson(jsonObject):
     analyzeTextWithNltk(jsonData['text'])
 
 def analyzeTextWithNltk(text):
-    print("------ NLTK started")
-    print(nltk.pos_tag(word_tokenize(text)))
-    print("------ Done!")
+    print("------ NLTK started:")
+    posTags = nltk.pos_tag(word_tokenize(text))
+    print(posTags)
+    print("------ Extracted Data:")
+    # Get all Nouns
+    countNouns = 0
+    for item in posTags:
+        if (item[1] == "NN"):
+            countNouns += 1
+    # Get all Verbs
+    countVerbs = 0
+    for item in posTags:
+        if (item[1] == "VB"):
+            countVerbs += 1
+    
+    print("Total no. of Nouns: " + str(countNouns))
+    print("Total no. of Verbs: " + str(countVerbs))
+
 
 def getWordFrequency(jsonObject):
     jsonData = json.loads(jsonObject)
@@ -44,3 +63,5 @@ def clusterJson():
 
 def getClusteredJson():
     return clusteredJson
+
+analyzeTextWithNltk(test_str)
